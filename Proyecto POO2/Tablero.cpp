167cba36dee6647f680f312794c313c7279ca3c3
LuisUTEC <luis.cordero@utec.edu.pe>
2019-06-24 21:56:17 -0500

    .

--- Proyecto POO2/Tablero.cpp
+++ Proyecto POO2/Tablero.cpp
@@ -8,10 +8,10 @@
 using namespace std;
 
 class Tablero{
+private:
+    Casilla* mesa[11][11];
 public:
     Tablero() {
-        Casilla* mesa[11][11];
-
         Casilla* Casa_1_amarillo = new CasillaCasa(1,1);
         Casilla* Casa_2_amarillo = new CasillaCasa(2,1);
         Casilla* Casa_3_amarillo = new CasillaCasa(1,2);
@@ -165,89 +165,21 @@ public:
     }
 
     void mostrar(){
-        sf::RenderWindow window(sf::VideoMode(1000, 650), "Ludo");
+        RenderWindow window(VideoMode(1000, 650), "Ludo");
         while (window.isOpen())
         {
             Event event;
             while (window.pollEvent(event))
             {
-                if (event.type == sf::Event::Closed)
+                if (event.type == Event::Closed)
                     window.close();
             }
             window.clear(sf::Color::White);
-            window.draw();
-            window.draw(Casa_2_amarillo);
-            window.draw(Casa_3_amarillo);
-            window.draw(Casa_4_amarillo);
-            window.draw(Casa_1_rojo);
-            window.draw(Casa_2_rojo);
-            window.draw(Casa_3_rojo);
-            window.draw(Casa_4_rojo);
-            window.draw(Casa_1_verde);
-            window.draw(Casa_2_verde);
-            window.draw(Casa_3_verde);
-            window.draw(Casa_4_verde);
-            window.draw(Casa_1_azul);
-            window.draw(Casa_2_azul);
-            window.draw(Casa_3_azul);
-            window.draw(Casa_4_azul);
-            window.draw(Recorrido1_1);
-            window.draw(Recorrido1_2);
-            window.draw(Recorrido1_3);
-            window.draw(Recorrido1_4);
-            window.draw(Recorrido1_5);
-            window.draw(Recorrido1_6);
-            window.draw(Recorrido1_7);
-            window.draw(Recorrido1_8);
-            window.draw(Recorrido1_9);
-            window.draw(Recorrido1_10);
-            window.draw(Recorrido2_1);
-            window.draw(Recorrido2_2);
-            window.draw(Recorrido2_3);
-            window.draw(Recorrido2_4);
-            window.draw(Recorrido2_5);
-            window.draw(Recorrido2_6);
-            window.draw(Recorrido2_7);
-            window.draw(Recorrido2_8);
-            window.draw(Recorrido2_9);
-            window.draw(Recorrido2_10);
-            window.draw(Recorrido3_1);
-            window.draw(Recorrido3_2);
-            window.draw(Recorrido3_3);
-            window.draw(Recorrido3_4);
-            window.draw(Recorrido3_5);
-            window.draw(Recorrido3_6);
-            window.draw(Recorrido3_7);
-            window.draw(Recorrido3_8);
-            window.draw(Recorrido4_1);
-            window.draw(Recorrido4_2);
-            window.draw(Recorrido4_3);
-            window.draw(Recorrido4_4);
-            window.draw(Recorrido4_5);
-            window.draw(Recorrido4_6);
-            window.draw(Recorrido4_7);
-            window.draw(Recorrido4_8);
-            window.draw(Inicio_amarillo);
-            window.draw(Inicio_rojo);
-            window.draw(Inicio_verde);
-            window.draw(Inicio_azul);
-            window.draw(SeguroVerde_1);
-            window.draw(SeguroVerde_2);
-            window.draw(SeguroVerde_3);
-            window.draw(SeguroRojo_1);
-            window.draw(SeguroRojo_2);
-            window.draw(SeguroRojo_3);
-            window.draw(SeguroAzul_1);
-            window.draw(SeguroAzul_2);
-            window.draw(SeguroAzul_3);
-            window.draw(SeguroAmarillo_1);
-            window.draw(SeguroAmarillo_2);
-            window.draw(SeguroAmarillo_3);
-            window.draw(DestinoFinal_Amarillo);
-            window.draw(DestinoFinal_Azul);
-            window.draw(DestinoFinal_Rojo);
-            window.draw(DestinoFinal_Verde);
-
+            for (int i = 0; i < 11; i++) {
+                for (int j = 0; j < 11; j++) {
+                    window.draw(mesa[i][j]->mostrarEstadoCasilla());
+                }
+            }
             window.display();
         }
     }
