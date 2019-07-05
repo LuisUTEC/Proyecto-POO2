7cbd52c5dfb736cea25c27cd888abd8afe8bb924
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-07-05 15:34:18 -0500

    Update Juego.cpp

--- Proyecto POO2/Juego.cpp
+++ Proyecto POO2/Juego.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Ficha.cpp"
 #include "Jugador.cpp"
+#include "Casilla.cpp"
 using namespace std;
 
 class Juego{
@@ -13,18 +14,59 @@ public:
 
     }
 
-    void siguienteTurno(){}
+    void siguienteTurno(){
+        Jugador turno;
+        vector<Jugador> Turnos;
+        turno[0]=Jugador(" ", 0);
+        turno[1]=Jugador(" ", 1);
+        turno[2]=Jugador(" ", 2);
+        turno[3]=Jugador(" ", 3);
+
+    Dado a;
+    for int(i=0;i<4;i++){
+
+        if (a.lanzar()!= 6){
+            turno=turno[i];
+        }
+    }
+
+    }
 
     void adiciionarJugador( string nombre , int color){}
 
     void validarFinJuego(){}
 
-    void validarSalidaFicha(){}
+    void validarSalidaFicha(){
+    Jugador uno;
+    if(uno.fichas.estado==CasillaZonaSegura.mostrarEstadoCasilla()){
+        uno.jugar();
+    }}
 
-    void validarCapturaFiha(){}
+    void validarCapturaFicha(){}
 
-    void finalizarJuego(){}
+    void finalizarJuego(){
+    //Si todas sus fichas estan en el destino final significa que gano
+Jugador uno;
+
+    if(uno.fichas.estado==CasillaDestinoFinal.mostrarEstadoCasilla()){
+sf::Font fuente;
+if (!font.loadFromFile("arial.ttf"))
+	{
+
+	}
+	sf::Text texto;
+	texto.setFont(fuente);
+	texto.setString("Ganaste");
+	texto.setColor(sf::Color::Red);
+	texto.setCharacterSize(120);
+	window.draw(texto);
+
+    }
+
+    }
 
 
 };
+
+
 
