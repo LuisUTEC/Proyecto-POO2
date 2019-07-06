5f75326f12621e8975de21fa2bb6b8ce1ce93124
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-07-05 22:28:56 -0500

    Update Juego.cpp

--- Proyecto POO2/Juego.cpp
+++ Proyecto POO2/Juego.cpp
@@ -12,17 +12,17 @@ private:
 public:
     void iniciarJuego(){
 
-    }
-
-    void siguienteTurno(){
-        Jugador turno;
         vector<Jugador> Turnos;
         turno[0]=Jugador(" ", 0);
         turno[1]=Jugador(" ", 1);
         turno[2]=Jugador(" ", 2);
         turno[3]=Jugador(" ", 3);
+        // Selecciona la primera ficha
+        Turnos[0].seleccionarFichaenJuego(0);
+    }
 
-    Dado a;
+    void siguienteTurno(){
+    // Si es diferente de 6, cambia de turno si no vuelve a lanzar
     for int(i=0;i<4;i++){
 
         if (a.lanzar()!= 6){
@@ -32,23 +32,56 @@ public:
 
     }
 
-    void adiciionarJugador( string nombre , int color){}
+    void adiciionarJugador( string nombre , int color){
+    Jugador nuevo;
+    nuevo(nombre,color);
 
-    void validarFinJuego(){}
+    }
+
+   bool validarFinJuego(){
+       //Si todas las fichas estan en el destino final
+     if(uno.fichas.estado==CasillaDestinoFinal.mostrarEstadoCasilla())
+        return true;
+     else return false;
+    }
 
     void validarSalidaFicha(){
-    Jugador uno;
-    if(uno.fichas.estado==CasillaZonaSegura.mostrarEstadoCasilla()){
-        uno.jugar();
-    }}
 
-    void validarCapturaFicha(){}
+        //Si es 6 sale , sino vuelve a probar
+    Jugador uno;
+    Dado uno;
+    if(uno.fichas.estado==CasillaZonaSegura.mostrarEstadoCasilla()){
+            if(uno.lanzar()==6)
+        uno.jugar();
+    }
+    else validarSalidaFicha();
+    }
+
+    void validarCapturaFicha(){
+    Casilla primero, segundo, tercero, cuarto;
+    int posactual[10][10];
+    // si las posiciones son iguales manda el segundo a la carcel
+
+    if (posactual[primero.getX][primero.Y]==posactual[segundo.getX][segundo.getY]){
+        segundo.setX(0);
+        segundo.setY(0);
+    }
+    if (posactual[primero.getX][primero.Y]==posactual[tercero.getX][tercero.getY]){
+        tercero.setX(0);
+        tercero.setY(0);
+    }
+    if (posactual[primero.getX][primero.Y]==posactual[cuarto.getX][cuarto.getY]){
+        cuarto.setX(0);
+        cuarto.setY(0);
+    }
+
+    }
 
     void finalizarJuego(){
     //Si todas sus fichas estan en el destino final significa que gano
 Jugador uno;
 
-    if(uno.fichas.estado==CasillaDestinoFinal.mostrarEstadoCasilla()){
+    if(validarFinJuego()==true){
 sf::Font fuente;
 if (!font.loadFromFile("arial.ttf"))
 	{
@@ -62,11 +95,9 @@ if (!font.loadFromFile("arial.ttf"))
 	window.draw(texto);
 
     }
-
+    }
     }
 
 
 };
-
-
 
